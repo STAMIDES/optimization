adc216132a20b1edd75582f4fdc56a77dc874a70
Juan Pablo Rubio <juan.rubio@streaver.com>
2025-06-29 00:37:05 -0300

    chore: rename OptimizationApplication to OptimizationEngine

--- engine/src/main/java/org/mides/optimization/OptimizationEngine.java
+++ engine/src/main/java/org/mides/optimization/OptimizationEngine.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OptimizationApplication {
+public class OptimizationEngine {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OptimizationApplication.class, args);
+		SpringApplication.run(OptimizationEngine.class, args);
 	}
 }
